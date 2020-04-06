e4e91ba013a1a69fa5fd529a9c354534956c56aa
Ari Trujillo Wesler <trujillorobnetta@gmail.com>
2020-04-06 16:43:00 -0700

    Hardcode knock custom admin site
    Updated to accommodate custom user model from knock

--- admin_tools/utils.py
+++ admin_tools/utils.py
@@ -29,26 +29,8 @@ def uniquify(value, seen_values):
 
 
 def get_admin_site(context=None, request=None):
-    dashboard_cls = getattr(
-        settings,
-        'ADMIN_TOOLS_INDEX_DASHBOARD',
-        'admin_tools.dashboard.dashboards.DefaultIndexDashboard'
-    )
-
-    if isinstance(dashboard_cls, dict):
-        if context:
-            request = context.get('request')
-        curr_url = request.path
-        for key in dashboard_cls:
-            mod, inst = key.rsplit('.', 1)
-            mod = import_module(mod)
-            admin_site = getattr(mod, inst)
-            admin_url = reverse('%s:index' % admin_site.name)
-            if curr_url.startswith(admin_url):
-                return admin_site
-    else:
-        return admin.site
-    raise ValueError('Admin site matching "%s" not found' % dashboard_cls)
+    from knock.admin import org_admin
+    return org_admin
 
 
 def get_admin_site_name(context):
